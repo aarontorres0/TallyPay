4765d47b318a4fb9321fbc7e8e372b47c48b4a60
Aaron Torres <aarontorres1423@gmail.com>
2023-01-17 13:39:01 -0600

    UI changes

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
     <div className="container">
       <h1>TallyUp</h1>
       <label>
-        Bill Amount:
+
         <input
           type="number"
           placeholder="Enter bill amount"
@@ -46,7 +46,6 @@ function App() {
       </label>
       <br />
       <label>
-        Tip Percentage:
         <div className="segmented-control-container">
           <div className="segmented-control">
             <button
@@ -85,15 +84,15 @@ function App() {
           </div>
         </div>
       </label>
+      <br />
+      <label>
+        Tip Amount: $
+        <span>{tipAmount}</span>
+      </label>
 
       <br />
       <label>
-        Tip Amount:
-        <span>{tipAmount}</span>
-      </label>
-      <br />
-      <label>
-        Total Amount:
+        Total Amount: $
         <span>{totalAmount}</span>
       </label>
     </div>
